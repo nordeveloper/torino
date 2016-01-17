6947cbde9cfca7f52ff92c70edb23918451b40f9
Rasul <rasuldev@gmail.com>
2016-01-17 12:20:29 +0300

    adding to cart styling

--- cart/cart.js
+++ cart/cart.js
@@ -1,31 +1,40 @@
-
 var Cart;
 if (!Cart) Cart = {};
-Cart.AddItem = function (id, quantity, done, fail)
+Cart.AddItem = function (id, quantity)
 {
-
     var jqxhr = $.post("cart/cart.php",
-        {'action': 'add2basket', 'id': id, 'quantity': quantity})
-        .done(function(data){
-           if (typeof(done)=='function')
-                done(data);
-        })
-        .fail(function(data){
-           if (typeof(fail) == 'function')
-               fail(data);
-        });
+        {'action': 'add2basket', 'id': id, 'quantity': quantity});
     return jqxhr;
 }
 
 Cart.Init = function()
 {
     $('a.addtocart').click(function(){
-        console.log("adding to cart");
         var addLink = this;
+        $(addLink).addClass("adding");
         var id = this.getAttribute('data-item-id');
-        Cart.AddItem(id,1,
-            function(data)
+        Cart.AddItem(id,1)
+            .done(function(data)
             {
+                console.log(data);
+                $(addLink).tooltip({title: "Добавлено в корзину", html: false, placement: "right", trigger: "manual", container: "body"}).tooltip('show');
+                setTimeout(function() {$(addLink).tooltip('destroy')},1000);
+            })
+            .fail(function(data)
+            {
+                console.log(data);
+                $(addLink).tooltip({title: "Ошибка добавления в корзину", html: false, placement: "right", trigger: "manual", container: "body"}).tooltip('show');
+                setTimeout(function() {$(addLink).tooltip('destroy')},1000);
+            })
+            .always(function(data){
+                $(addLink).removeClass("adding");
+                $(addLink).blur();
+            });
+    });
+}
+
+
+// Popup window
 //					var dialogParams =
 //					{
 //						'title': 'Элемент добавлен',
@@ -33,28 +42,17 @@ Cart.Init = function()
 //						'width': 500,
 //						'height': 200
 //					}
-                //new BX.PopupWindow(dialogParams).Show();
-                //var oPopup = new BX.PopupWindow('add_to_basket', window.body, {
-                //    autoHide : true,
-                //    offsetTop : 1,
-                //    offsetLeft : 0,
-                //    lightShadow : true,
-                //    closeIcon : true,
-                //    closeByEsc : true,
-                //    overlay: {
-                //        backgroundColor: 'gray', opacity: '80'
-                //    }
-                //})
-                //oPopup.setContent('<div class="add-to-basket-popup">Элемент ' + id + ' добавлен в корзину</div>');
-                //oPopup.show();
-
-                $(addLink).tooltip({title: "Добавлен", placement: "right", trigger: "manual", container: "body"}).tooltip('show');
-                setTimeout(function() {$(addLink).tooltip('destroy')},1000);
-            },
-            function(data)
-            {
-
-            }
-        );
-    });
-}
+//new BX.PopupWindow(dialogParams).Show();
+//var oPopup = new BX.PopupWindow('add_to_basket', window.body, {
+//    autoHide : true,
+//    offsetTop : 1,
+//    offsetLeft : 0,
+//    lightShadow : true,
+//    closeIcon : true,
+//    closeByEsc : true,
+//    overlay: {
+//        backgroundColor: 'gray', opacity: '80'
+//    }
+//})
+//oPopup.setContent('<div class="add-to-basket-popup">Элемент ' + id + ' добавлен в корзину</div>');
+//oPopup.show();
