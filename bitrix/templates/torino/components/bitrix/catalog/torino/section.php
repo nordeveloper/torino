6aa90603e4810d78bf6a757e2add59f55150d5b5
ismag <ismag@example.com>
2016-01-18 02:28:55 +0300

    wrking on catalog

--- bitrix/templates/torino/components/bitrix/catalog/torino/section.php
+++ bitrix/templates/torino/components/bitrix/catalog/torino/section.php
@@ -159,29 +159,36 @@ if ($verticalGrid)
 	$component,
 	array("HIDE_ICONS" => "Y")
 );?>
-
+<?echo $arParams["SECTION_TOP_DEPTH"];?>
 <div class="divider" id="secdetsdiv"></div>
 
-<?$APPLICATION->IncludeComponent(
-    "bitrix:catalog.section.list",
-    "",
-    array(
-        "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-        "IBLOCK_ID" => $arParams["IBLOCK_ID"],
-        "CACHE_TYPE" => $arParams["CACHE_TYPE"],
-        "CACHE_TIME" => $arParams["CACHE_TIME"],
-        "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-        "COUNT_ELEMENTS" => $arParams["SECTION_COUNT_ELEMENTS"],
-        "TOP_DEPTH" => $arParams["SECTION_TOP_DEPTH"],
-        "SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
-        "VIEW_MODE" => $arParams["SECTIONS_VIEW_MODE"],
-        "SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
-        "HIDE_SECTION_NAME" => (isset($arParams["SECTIONS_HIDE_SECTION_NAME"]) ? $arParams["SECTIONS_HIDE_SECTION_NAME"] : "N"),
-        "ADD_SECTIONS_CHAIN" => (isset($arParams["ADD_SECTIONS_CHAIN"]) ? $arParams["ADD_SECTIONS_CHAIN"] : '')
-    ),
-    $component,
-    array("HIDE_ICONS" => "Y")
-);?>
+
+<? //If there's no subcategories, let's roll out main categories
+$secid = CIBlockFindTools::GetSectionID($secid, $arResult["VARIABLES"]["SECTION_CODE"], $arFlt);
+$arFlt = Array("IBLOCK_ID" => $arParams["IBLOCK_ID"], "SECTION_ID" => $secid);
+
+if (CIBlockSection::GetCount($arFlt)  <= 0) {
+ $APPLICATION->IncludeComponent(
+     "bitrix:catalog.section.list",
+     "",
+     array(
+         "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+         "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+         "CACHE_TYPE" => $arParams["CACHE_TYPE"],
+         "CACHE_TIME" => $arParams["CACHE_TIME"],
+         "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+         "COUNT_ELEMENTS" => $arParams["SECTION_COUNT_ELEMENTS"],
+         "TOP_DEPTH" => $arParams["SECTION_TOP_DEPTH"],
+         "SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
+         "VIEW_MODE" => $arParams["SECTIONS_VIEW_MODE"],
+         "SHOW_PARENT_NAME" => $arParams["SECTIONS_SHOW_PARENT_NAME"],
+         "HIDE_SECTION_NAME" => (isset($arParams["SECTIONS_HIDE_SECTION_NAME"]) ? $arParams["SECTIONS_HIDE_SECTION_NAME"] : "N"),
+         "ADD_SECTIONS_CHAIN" => (isset($arParams["ADD_SECTIONS_CHAIN"]) ? $arParams["ADD_SECTIONS_CHAIN"] : '')
+     ),
+     $component,
+     array("HIDE_ICONS" => "Y")
+ );
+}?>
 
 
 <?if($arParams["USE_COMPARE"]=="Y")
