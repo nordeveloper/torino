549c6a7c6e6ddfbf5778ee44dcb802aaf77b33c8
De4Dp007 <yangchenfu2@gmail.com>
2016-01-26 11:06:09 +0300

    soc butts - google analit commented

--- bitrix/templates/torino/header.php
+++ bitrix/templates/torino/header.php
@@ -43,6 +43,22 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
         <!--Spin Edit -->
         <link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH?>/css/jquery.bootstrap-touchspin.css" />
         <script type="text/javascript" src="<?=SITE_TEMPLATE_PATH?>/js/jquery.bootstrap-touchspin.js"></script>
+
+
+
+        <!-- Google Analytics -- >
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-XXXXX-Y', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <!-- End Google Analytics -->
+
+
 	</HEAD>
 
 	<BODY>
