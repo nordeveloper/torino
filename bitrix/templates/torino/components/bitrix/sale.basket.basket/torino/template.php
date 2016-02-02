35e9c7c8f9913576f12e1044990bd3e1334bd4d8
De4Dp007 <yangchenfu2@gmail.com>
2016-02-02 13:42:39 +0300

    wrknig on basket

--- bitrix/templates/torino/components/bitrix/sale.basket.basket/torino/template.php
+++ bitrix/templates/torino/components/bitrix/sale.basket.basket/torino/template.php
@@ -49,6 +49,56 @@ if (strlen($arResult["ERROR_MESSAGE"]) <= 0)
 	$naHidden = ($naCount == 0) ? "style=\"display:none\"" : "";
 
 	?>
+
+    <div class="container" id="userbasket">
+        <div class="row">
+            <div class="col-md-12">
+            <form method="post" action="<?=POST_FORM_ACTION_URI?>" name="basket_form" id="basket_form">
+                <div class="panel panel-primary" id="basket_form_container">
+                    <div class="panel-heading" id="baskethead">
+                        <h3 class="panel-title">
+                            <span>
+                                Корзина пользователя &nbsp;&nbsp;
+							    <strong><a id="usname" href="/personal/index.php"><? echo CUser::GetFullName() ?></a></strong><?//=GetMessage("SALE_ITEMS")?>
+                            </span>
+                        </h3>
+                        <span class="pull-right">
+                            <!-- Tabs -->
+                            <ul class="nav panel-tabs">
+                                <li class="active"><a href="#tab1" data-toggle="tab" id="basket_toolbar_button" class="ordbutts current" onclick="showBasketItemsList()"><?=GetMessage("SALE_BASKET_ITEMS")?><div id="normal_count" class="flat" style="display:none">&nbsp;(<?=$normalCount?>)</div></a></li>
+                                <li><a href="#tab2" data-toggle="tab" id="basket_toolbar_button_delayed" class="ordbutts" onclick="showBasketItemsList(2)" <?=$delayHidden?>><?=GetMessage("SALE_BASKET_ITEMS_DELAYED")?><div id="delay_count" class="flat">&nbsp;(<?=$delayCount?>)</div></a></li>
+                                <li><a href="#tab3" data-toggle="tab" id="basket_toolbar_button_subscribed" class="ordbutts" onclick="showBasketItemsList(3)" <?=$subscribeHidden?>><?=GetMessage("SALE_BASKET_ITEMS_SUBSCRIBED")?><div id="subscribe_count" class="flat">&nbsp;(<?=$subscribeCount?>)</div></a></li>
+                                <li><a href="#tab4" data-toggle="tab" id="basket_toolbar_button_not_available" class="ordbutts" onclick="showBasketItemsList(4)" <?=$naHidden?>><?=GetMessage("SALE_BASKET_ITEMS_NOT_AVAILABLE")?><div id="not_available_count" class="flat">&nbsp;(<?=$naCount?>)</div></a></li>
+                            </ul>
+                        </span>
+                    </div>
+                    <div class="panel-body" id="basketbody">
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="tab1">
+                                <?include($_SERVER["DOCUMENT_ROOT"].$templateFolder."/basket_items.php");?>
+                            </div>
+                            <div class="tab-pane" id="tab2">
+                                <?include($_SERVER["DOCUMENT_ROOT"].$templateFolder."/basket_items_delayed.php");?>
+                            </div>
+                            <div class="tab-pane" id="tab3">
+                                <?include($_SERVER["DOCUMENT_ROOT"].$templateFolder."/basket_items_subscribed.php");?>
+                            </div>
+                            <div class="tab-pane" id="tab4">
+                                <?include($_SERVER["DOCUMENT_ROOT"].$templateFolder."/basket_items_not_available.php");?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <input type="hidden" name="BasketOrder" value="BasketOrder" />
+                    <!-- <input type="hidden" name="ajax_post" id="ajax_post" value="Y"> -->
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+
+
+<?/*
 	<div class="row" id="basketbasket">
 		<form method="post" action="<?=POST_FORM_ACTION_URI?>" name="basket_form" id="basket_form">
 			<div id="basket_form_container">
@@ -78,10 +128,10 @@ if (strlen($arResult["ERROR_MESSAGE"]) <= 0)
 			<!-- <input type="hidden" name="ajax_post" id="ajax_post" value="Y"> -->
 		</form>
     </div>
-	<?
-}
-else
-{
+*/?>
+
+
+
+<?} else {
 	ShowError($arResult["ERROR_MESSAGE"]);
-}
-?>
+} ?>
