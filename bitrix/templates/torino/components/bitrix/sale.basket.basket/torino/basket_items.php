b6ed1da117727261bc0456211d3e096b1bad099b
De4Dp007 <yangchenfu2@gmail.com>
2016-02-04 13:32:26 +0300

    still wrking on basket

--- bitrix/templates/torino/components/bitrix/sale.basket.basket/torino/basket_items.php
+++ bitrix/templates/torino/components/bitrix/sale.basket.basket/torino/basket_items.php
@@ -12,407 +12,84 @@ if ($normalCount > 0):
 ?>
 <div id="basket_items_list">
 	<div class="bx_ordercart_order_table_container">
-		<table id="basket_items">
+		<table id="basket_items" class="table-responsive col-md-12">
 			<thead>
 				<tr>
-					<td class="margin"></td>
-					<?
-					foreach ($arResult["GRID"]["HEADERS"] as $id => $arHeader):
-						$arHeader["name"] = (isset($arHeader["name"]) ? (string)$arHeader["name"] : '');
-						if ($arHeader["name"] == '')
-							$arHeader["name"] = GetMessage("SALE_".$arHeader["id"]);
-						$arHeaders[] = $arHeader["id"];
+					<td class="itemphoto xs-invisible"></td>
 
-						// remember which values should be shown not in the separate columns, but inside other columns
-						if (in_array($arHeader["id"], array("TYPE")))
-						{
-							$bPriceType = true;
-							continue;
-						}
-						elseif ($arHeader["id"] == "PROPS")
-						{
-							$bPropsColumn = true;
-							continue;
-						}
-						elseif ($arHeader["id"] == "DELAY")
-						{
-							$bDelayColumn = true;
-							continue;
-						}
-						elseif ($arHeader["id"] == "DELETE")
-						{
-							$bDeleteColumn = true;
-							continue;
-						}
-						elseif ($arHeader["id"] == "WEIGHT")
-						{
-							$bWeightColumn = true;
-						}
+					<td class="item" id="col_Name">
+                        Наименование блюда
+                    </td>
 
-						if ($arHeader["id"] == "NAME"):
-						?>
-							<td class="item" colspan="2" id="col_<?=$arHeader["id"];?>">
-						<?
-						elseif ($arHeader["id"] == "PRICE"):
-						?>
-							<td class="price" id="col_<?=$arHeader["id"];?>">
-						<?
-						else:
-						?>
-							<td class="custom" id="col_<?=$arHeader["id"];?>">
-						<?
-						endif;
-						?>
-							<?=$arHeader["name"]; ?>
-							</td>
-					<?
-					endforeach;
+                    <td id="col_Controls"></td>
 
-					if ($bDeleteColumn || $bDelayColumn):
-					?>
-						<td class="custom"></td>
-					<?
-					endif;
-					?>
-						<td class="margin"></td>
+                    <td id="col_Price">Цена</td>
+
+                    <td id="col_Quant">Количество</td>
+
+                    <td id="col_SumPrice">Стоимость</td>
+
 				</tr>
 			</thead>
 
 			<tbody>
-				<?
-				foreach ($arResult["GRID"]["ROWS"] as $k => $arItem):
+            <?foreach ($arResult["GRID"]["ROWS"] as $k => $arItem):
+                if ($arItem["DELAY"] == "N" && $arItem["CAN_BUY"] == "Y"):?>
+                    <tr id="<?=$arItem["ID"]?>">
 
-					if ($arItem["DELAY"] == "N" && $arItem["CAN_BUY"] == "Y"):
-				?>
-					<tr id="<?=$arItem["ID"]?>">
-						<td class="margin"></td>
-						<?
-						foreach ($arResult["GRID"]["HEADERS"] as $id => $arHeader):
 
-							if (in_array($arHeader["id"], array("PROPS", "DELAY", "DELETE", "TYPE"))) // some values are not shown in the columns in this template
-								continue;
+                        <td class="itemphoto"></td>
 
-							if ($arHeader["id"] == "NAME"):
-							?>
-								<td class="itemphoto">
-									<div class="bx_ordercart_photo_container">
-										<?
-										if (strlen($arItem["PREVIEW_PICTURE_SRC"]) > 0):
-											$url = $arItem["PREVIEW_PICTURE_SRC"];
-										elseif (strlen($arItem["DETAIL_PICTURE_SRC"]) > 0):
-											$url = $arItem["DETAIL_PICTURE_SRC"];
-										else:
-											$url = $templateFolder."/images/no_photo.png";
-										endif;
-										?>
+                        <td class="item" colspan="" class="item">
+                            <h2 class="bx_ordercart_itemtitle">
+                                <?if (strlen($arItem["DETAIL_PAGE_URL"]) > 0):?><a href="<?=$arItem["DETAIL_PAGE_URL"] ?>"><?endif;?>
+                                    <?=$arItem["NAME"]?>
+                                    <?if (strlen($arItem["DETAIL_PAGE_URL"]) > 0):?></a><?endif;?>
+                            </h2>
 
-										<?if (strlen($arItem["DETAIL_PAGE_URL"]) > 0):?><a href="<?=$arItem["DETAIL_PAGE_URL"] ?>"><?endif;?>
-											<div class="bx_ordercart_photo" style="background-image:url('<?=$url?>')"></div>
-										<?if (strlen($arItem["DETAIL_PAGE_URL"]) > 0):?></a><?endif;?>
-									</div>
-									<?
-									if (!empty($arItem["BRAND"])):
-									?>
-									<div class="bx_ordercart_brand">
-										<img alt="" src="<?=$arItem["BRAND"]?>" />
-									</div>
-									<?
-									endif;
-									?>
-								</td>
-								<td class="item">
-									<h2 class="bx_ordercart_itemtitle">
-										<?if (strlen($arItem["DETAIL_PAGE_URL"]) > 0):?><a href="<?=$arItem["DETAIL_PAGE_URL"] ?>"><?endif;?>
-											<?=$arItem["NAME"]?>
-										<?if (strlen($arItem["DETAIL_PAGE_URL"]) > 0):?></a><?endif;?>
-									</h2>
-									<div class="bx_ordercart_itemart">
-										<?
-										if ($bPropsColumn):
-											foreach ($arItem["PROPS"] as $val):
+                            <?if (isset($arItem["PROPERTY_CONSIST_VALUE"]) && ($arItem["PROPERTY_CONSIST_VALUE"] != "")):?>
+                                <p class="description">
+                                    <?=$arItem["PROPERTY_CONSIST_VALUE"]?>.
+                                </p>
+                            <?endif;?>
 
-												if (is_array($arItem["SKU_DATA"]))
-												{
-													$bSkip = false;
-													foreach ($arItem["SKU_DATA"] as $propId => $arProp)
-													{
-														if ($arProp["CODE"] == $val["CODE"])
-														{
-															$bSkip = true;
-															break;
-														}
-													}
-													if ($bSkip)
-														continue;
-												}
+                            <?if (isset($arItem["PROPERTY_NUTRITION_VALUE"]) && ($arItem["PROPERTY_NUTRITION_VALUE"] != "")):?>
+                                <p class="nutrition">
+                                    Сведения о пищевой ценности (на 100 гр.): <?=$arItem["PROPERTY_NUTRITION_VALUE"]?>.
+                                </p>
+                            <?endif;?>
 
-												echo $val["NAME"].":&nbsp;<span>".$val["VALUE"]."<span><br/>";
-											endforeach;
-										endif;
-										?>
-									</div>
-									<?
-									if (is_array($arItem["SKU_DATA"]) && !empty($arItem["SKU_DATA"])):
-										foreach ($arItem["SKU_DATA"] as $propId => $arProp):
+                            <?if (isset($arItem["PROPERTY_CALORIES_VALUE"]) && ($arItem["PROPERTY_CALORIES_VALUE"] != "")):?>
+                                <p class="nutrition">
+                                    Калорийность (на 100 гр.): <?=$arItem["PROPERTY_CALORIES_VALUE"]?>.
+                                </p>
+                            <?endif;?>
 
-											// if property contains images or values
-											$isImgProperty = false;
-											if (array_key_exists('VALUES', $arProp) && is_array($arProp["VALUES"]) && !empty($arProp["VALUES"]))
-											{
-												foreach ($arProp["VALUES"] as $id => $arVal)
-												{
-													if (isset($arVal["PICT"]) && !empty($arVal["PICT"]) && is_array($arVal["PICT"])
-														&& isset($arVal["PICT"]['SRC']) && !empty($arVal["PICT"]['SRC']))
-													{
-														$isImgProperty = true;
-														break;
-													}
-												}
-											}
-											$countValues = count($arProp["VALUES"]);
-											$full = ($countValues > 5) ? "full" : "";
+                            <?//test_dump($arItem)?>
+                        </td>
 
-											if ($isImgProperty): // iblock element relation property
-											?>
-												<div class="bx_item_detail_scu_small_noadaptive <?=$full?>">
+                        <td class="control">
+                            <a href="#" class="btn btn-danger btn-lg">
+                                <span class="glyphicon glyphicon-remove"></span> Удалить
+                            </a>
+                        </td>
 
-													<span class="bx_item_section_name_gray">
-														<?=$arProp["NAME"]?>:
-													</span>
+                        <td  class="price">
+                            <?=$arItem["FULL_PRICE_FORMATED"]?>
+                        </td>
 
-													<div class="bx_scu_scroller_container">
+                        <td  class="quantity">
+                            <?=$arItem["QUANTITY"]?>
+                        </td>
 
-														<div class="bx_scu">
-															<ul id="prop_<?=$arProp["CODE"]?>_<?=$arItem["ID"]?>"
-																style="width: 200%; margin-left:0%;"
-																class="sku_prop_list"
-																>
-																<?
-																foreach ($arProp["VALUES"] as $valueId => $arSkuValue):
+                        <td  class="sumprice">
+                            <?=$arItem["SUM"]?>
+                        </td>
 
-																	$selected = "";
-																	foreach ($arItem["PROPS"] as $arItemProp):
-																		if ($arItemProp["CODE"] == $arItem["SKU_DATA"][$propId]["CODE"])
-																		{
-																			if ($arItemProp["VALUE"] == $arSkuValue["NAME"] || $arItemProp["VALUE"] == $arSkuValue["XML_ID"])
-																				$selected = "bx_active";
-																		}
-																	endforeach;
-																?>
-																	<li style="width:10%;"
-																		class="sku_prop <?=$selected?>"
-																		data-value-id="<?=$arSkuValue["XML_ID"]?>"
-																		data-element="<?=$arItem["ID"]?>"
-																		data-property="<?=$arProp["CODE"]?>"
-																		>
-																		<a href="javascript:void(0);">
-																			<span style="background-image:url(<?=$arSkuValue["PICT"]["SRC"]?>)"></span>
-																		</a>
-																	</li>
-																<?
-																endforeach;
-																?>
-															</ul>
-														</div>
+                <? endif;
+            endforeach;?>
 
-														<div class="bx_slide_left" onclick="leftScroll('<?=$arProp["CODE"]?>', <?=$arItem["ID"]?>, <?=$countValues?>);"></div>
-														<div class="bx_slide_right" onclick="rightScroll('<?=$arProp["CODE"]?>', <?=$arItem["ID"]?>, <?=$countValues?>);"></div>
-													</div>
 
-												</div>
-											<?
-											else:
-											?>
-												<div class="bx_item_detail_size_small_noadaptive <?=$full?>">
-
-													<span class="bx_item_section_name_gray">
-														<?=$arProp["NAME"]?>:
-													</span>
-
-													<div class="bx_size_scroller_container">
-														<div class="bx_size">
-															<ul id="prop_<?=$arProp["CODE"]?>_<?=$arItem["ID"]?>"
-																style="width: 200%; margin-left:0%;"
-																class="sku_prop_list"
-																>
-																<?
-																foreach ($arProp["VALUES"] as $valueId => $arSkuValue):
-
-																	$selected = "";
-																	foreach ($arItem["PROPS"] as $arItemProp):
-																		if ($arItemProp["CODE"] == $arItem["SKU_DATA"][$propId]["CODE"])
-																		{
-																			if ($arItemProp["VALUE"] == $arSkuValue["NAME"])
-																				$selected = "bx_active";
-																		}
-																	endforeach;
-																?>
-																	<li style="width:10%;"
-																		class="sku_prop <?=$selected?>"
-																		data-value-id="<?=$arSkuValue["NAME"]?>"
-																		data-element="<?=$arItem["ID"]?>"
-																		data-property="<?=$arProp["CODE"]?>"
-																		>
-																		<a href="javascript:void(0);"><?=$arSkuValue["NAME"]?></a>
-																	</li>
-																<?
-																endforeach;
-																?>
-															</ul>
-														</div>
-														<div class="bx_slide_left" onclick="leftScroll('<?=$arProp["CODE"]?>', <?=$arItem["ID"]?>, <?=$countValues?>);"></div>
-														<div class="bx_slide_right" onclick="rightScroll('<?=$arProp["CODE"]?>', <?=$arItem["ID"]?>, <?=$countValues?>);"></div>
-													</div>
-
-												</div>
-											<?
-											endif;
-										endforeach;
-									endif;
-									?>
-								</td>
-							<?
-							elseif ($arHeader["id"] == "QUANTITY"):
-							?>
-								<td class="custom">
-									<span><?=$arHeader["name"]; ?>:</span>
-									<div class="items-centered">
-										<table cellspacing="0" cellpadding="0" class="counter">
-											<tr>
-												<td>
-													<?
-													$ratio = isset($arItem["MEASURE_RATIO"]) ? $arItem["MEASURE_RATIO"] : 0;
-													$max = isset($arItem["AVAILABLE_QUANTITY"]) ? "max=\"".$arItem["AVAILABLE_QUANTITY"]."\"" : "";
-													$useFloatQuantity = ($arParams["QUANTITY_FLOAT"] == "Y") ? true : false;
-													$useFloatQuantityJS = ($useFloatQuantity ? "true" : "false");
-													?>
-													<input
-														type="text"
-														size="3"
-														id="QUANTITY_INPUT_<?=$arItem["ID"]?>"
-														name="QUANTITY_INPUT_<?=$arItem["ID"]?>"
-														size="2"
-														maxlength="18"
-														min="0"
-														<?=$max?>
-														step="<?=$ratio?>"
-														style="max-width: 50px"
-														value="<?=$arItem["QUANTITY"]?>"
-														onchange="updateQuantity('QUANTITY_INPUT_<?=$arItem["ID"]?>', '<?=$arItem["ID"]?>', <?=$ratio?>, <?=$useFloatQuantityJS?>)"
-													>
-												</td>
-												<?
-												if (!isset($arItem["MEASURE_RATIO"]))
-												{
-													$arItem["MEASURE_RATIO"] = 1;
-												}
-
-												if (
-													floatval($arItem["MEASURE_RATIO"]) != 0
-												):
-												?>
-													<td id="basket_quantity_control">
-														<div class="basket_quantity_control">
-															<a href="javascript:void(0);" class="plus" onclick="setQuantity(<?=$arItem["ID"]?>, <?=$arItem["MEASURE_RATIO"]?>, 'up', <?=$useFloatQuantityJS?>);"></a>
-															<a href="javascript:void(0);" class="minus" onclick="setQuantity(<?=$arItem["ID"]?>, <?=$arItem["MEASURE_RATIO"]?>, 'down', <?=$useFloatQuantityJS?>);"></a>
-														</div>
-													</td>
-												<?
-												endif;
-												if (isset($arItem["MEASURE_TEXT"]))
-												{
-													?>
-														<td style="text-align: left"><?=$arItem["MEASURE_TEXT"]?></td>
-													<?
-												}
-												?>
-											</tr>
-										</table>
-									</div>
-									<input type="hidden" id="QUANTITY_<?=$arItem['ID']?>" name="QUANTITY_<?=$arItem['ID']?>" value="<?=$arItem["QUANTITY"]?>" />
-								</td>
-							<?
-							elseif ($arHeader["id"] == "PRICE"):
-							?>
-								<td class="price">
-										<div class="current_price" id="current_price_<?=$arItem["ID"]?>">
-											<?=$arItem["PRICE_FORMATED"]?>
-										</div>
-										<div class="old_price" id="old_price_<?=$arItem["ID"]?>">
-											<?if (floatval($arItem["DISCOUNT_PRICE_PERCENT"]) > 0):?>
-												<?=$arItem["FULL_PRICE_FORMATED"]?>
-											<?endif;?>
-										</div>
-
-									<?if ($bPriceType && strlen($arItem["NOTES"]) > 0):?>
-										<div class="type_price"><?=GetMessage("SALE_TYPE")?></div>
-										<div class="type_price_value"><?=$arItem["NOTES"]?></div>
-									<?endif;?>
-								</td>
-							<?
-							elseif ($arHeader["id"] == "DISCOUNT"):
-							?>
-								<td class="custom">
-									<span><?=$arHeader["name"]; ?>:</span>
-									<div id="discount_value_<?=$arItem["ID"]?>"><?=$arItem["DISCOUNT_PRICE_PERCENT_FORMATED"]?></div>
-								</td>
-							<?
-							elseif ($arHeader["id"] == "WEIGHT"):
-							?>
-								<td class="custom">
-									<span><?=$arHeader["name"]; ?>:</span>
-									<?=$arItem["WEIGHT_FORMATED"]?>
-								</td>
-							<?
-							else:
-							?>
-								<td class="custom">
-									<span><?=$arHeader["name"]; ?>:</span>
-									<?
-									if ($arHeader["id"] == "SUM"):
-									?>
-										<div id="sum_<?=$arItem["ID"]?>">
-									<?
-									endif;
-
-									echo $arItem[$arHeader["id"]];
-
-									if ($arHeader["id"] == "SUM"):
-									?>
-										</div>
-									<?
-									endif;
-									?>
-								</td>
-							<?
-							endif;
-						endforeach;
-
-						if ($bDelayColumn || $bDeleteColumn):
-						?>
-							<td class="control">
-								<?
-								if ($bDeleteColumn):
-								?>
-									<a href="<?=str_replace("#ID#", $arItem["ID"], $arUrls["delete"])?>"><?=GetMessage("SALE_DELETE")?></a><br />
-								<?
-								endif;
-								if ($bDelayColumn):
-								?>
-									<a href="<?=str_replace("#ID#", $arItem["ID"], $arUrls["delay"])?>"><?=GetMessage("SALE_DELAY")?></a>
-								<?
-								endif;
-								?>
-							</td>
-						<?
-						endif;
-						?>
-							<td class="margin"></td>
-					</tr>
-					<?
-					endif;
-				endforeach;
-				?>
 			</tbody>
 		</table>
 	</div>
